a7e31f9c84f00d40f29253ce0006cfc3613391a7
Natan Shames <natanshames@MacBook-Pro-Natan.local>
2024-01-11 20:44:45 +0200

    Initial commit

---
+++ src/personGenerator.js
@@ -0,0 +1,18 @@
+import * as Const from "./constans";
+
+const generator = () => {
+    const resArr = [];
+    for (let i = 0; i < Const.NUMBER; i++) {
+        const randomAge = Math.floor(
+            Math.random() * (Const.MAX_AGE - Const.MIN_AGE) + Const.MIN_AGE);
+        resArr.push({
+            personName: "John",
+            age: randomAge,
+            city: "Haifa"
+        })
+    }
+    return resArr;
+}
+export const DEFAULT_NAME = "Bill";
+
+export default generator;
